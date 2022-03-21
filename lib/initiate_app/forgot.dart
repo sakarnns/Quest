c3f5db41f099a7939b9a5fc5d9fab50b1774c707
Sagarn Apichitsomboon <61071507+sakarnns@users.noreply.github.com>
2022-03-21 14:59:22 +0700

    Redesign UI 15:00

--- lib/initiate_app/forgot.dart
+++ lib/initiate_app/forgot.dart
@@ -50,113 +50,116 @@ class _ForgotPageState extends State<ForgotPage> {
           backgroundColor: Colors.transparent,
           elevation: 0.0,
         ),
-        body: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: width(context: context) / 20,
-          ),
-          child: Column(
-            children: [
-              Container(
-                height: 100,
-                width: 166.66,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage(
-                          'assets/images/quest_newlogo_color.png'), // Add Path image
-                      fit: BoxFit.fill),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(
-                    top: MediaQuery.of(context).size.height / 33),
-                child: Text(
-                  'Forgot Password',
-                  style: TextStyle(
-                    fontSize: 40.0,
-                    fontWeight: FontWeight.w700,
-                    color: Color(0xFF6F2DA8),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: width(context: context) / 20,
+            ),
+            child: Column(
+              children: [
+                Container(
+                  height: 100,
+                  width: 166.66,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage(
+                            'assets/images/quest_newlogo_color.png'), // Add Path image
+                        fit: BoxFit.fill),
                   ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(
-                    top: MediaQuery.of(context).size.height / 50),
-                child: Text(
-                  'Enter the email address associated with your account.',
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.w200,
+                Padding(
+                  padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height / 33),
+                  child: Text(
+                    'Forgot Password',
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.w700,
+                      color: Color(0xFF6F2DA8),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: height(context: context) / 100,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  vertical: height(context: context) / 50,
+                Padding(
+                  padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height / 50),
+                  child: Text(
+                    'Enter the email address associated with your account.',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.w200,
+                    ),
+                  ),
                 ),
-                child: TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                        errorText: isvalid == null
-                            ? null
-                            : isvalid!
-                                ? null
-                                : "Please enter a valid email",
-                        focusedBorder: outlineInputBorder(),
-                        filled: true,
-                        fillColor: Colors.transparent,
-                        hintText: 'E-mail',
-                        contentPadding: EdgeInsets.all(8.0),
-                        border: outlineInputBorder()),
-                    onChanged: (val) {
-                      if (val.isEmpty ||
-                          EmailValidator.validate(
-                            val,
-                          )) {
-                        setState(() {
-                          emailaddress = val;
-                          isvalid = true;
-                        });
-                      } else {
-                        setState(() {
-                          emailaddress = val;
-                          isvalid = false;
-                        });
-                      }
+                SizedBox(
+                  height: height(context: context) / 100,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: height(context: context) / 50,
+                  ),
+                  child: TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                          errorText: isvalid == null
+                              ? null
+                              : isvalid!
+                                  ? null
+                                  : "Please enter a valid email",
+                          focusedBorder: outlineInputBorder(),
+                          filled: true,
+                          fillColor: Colors.transparent,
+                          hintText: 'E-mail',
+                          contentPadding: EdgeInsets.all(8.0),
+                          border: outlineInputBorder()),
+                      onChanged: (val) {
+                        if (val.isEmpty ||
+                            EmailValidator.validate(
+                              val,
+                            )) {
+                          setState(() {
+                            emailaddress = val;
+                            isvalid = true;
+                          });
+                        } else {
+                          setState(() {
+                            emailaddress = val;
+                            isvalid = false;
+                          });
+                        }
 
-                      // print(isvalid);
-                    }),
-              ),
-              SizedBox(
-                height: height(context: context) / 20,
-              ),
-              MaterialButton(
-                color: Color(0xFF6F2DA8),
-                disabledColor: Colors.grey,
-                minWidth: 163,
-                height: 40,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8)),
-                onPressed: isvalid != false &&
-                        emailaddress != null &&
-                        emailaddress != ""
-                    ? () {
-                        forgetpass(emailaddress!);
-                        print(emailaddress);
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => VerifyOTP()),
-                        );
-                      }
-                    : null,
-                child: Text(
-                  'Send OTP',
-                  style: TextStyle(color: Colors.white, fontSize: 16),
+                        // print(isvalid);
+                      }),
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: height(context: context) / 20,
+                ),
+                MaterialButton(
+                  color: Color(0xFF6F2DA8),
+                  disabledColor: Colors.grey,
+                  minWidth: 163,
+                  height: 40,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  onPressed: isvalid != false &&
+                          emailaddress != null &&
+                          emailaddress != ""
+                      ? () {
+                          forgetpass(emailaddress!);
+                          print(emailaddress);
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => VerifyOTP()),
+                          );
+                        }
+                      : null,
+                  child: Text(
+                    'Send OTP',
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
