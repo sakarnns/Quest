242750186e692992e109bf3abf3cf7f57eb9748e
Sagarn Apichitsomboon <61071507+sakarnns@users.noreply.github.com>
2022-03-24 14:52:37 +0700

    Add set new password page

---
+++ lib/initiate_app/setnewpassword.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:quest_2/styles/input_border.dart';
+import 'package:quest_2/styles/size.dart';
+
+import 'login.dart';
+
+class SetNewPassword extends StatefulWidget {
+  SetNewPassword({Key? key}) : super(key: key);
+
+  @override
+  State<SetNewPassword> createState() => _SetNewPasswordState();
+}
+
+bool? isvalid;
+String? confirmpasswordOnForgot;
+String? passwordOnForgot;
+bool? isPasswordMatch;
+
+class _SetNewPasswordState extends State<SetNewPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      behavior: HitTestBehavior.translucent,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: width(context: context) / 20,
+            ),
+            child: Column(
+              children: [
+                Container(
+                  height: 100,
+                  width: 166.66,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage(
+                            'assets/images/quest_newlogo_color.png'), // Add Path image
+                        fit: BoxFit.fill),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height / 50),
+                  child: Text(
+                    'Create new password',
+                    style: TextStyle(
+                      fontSize: 32.0,
+                      fontWeight: FontWeight.w700,
+                      color: Color(0xFF6F2DA8),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height / 70),
+                  child: Text(
+                    'Your new password must be different from previous used \n                                               passwords.',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.w200,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: height(context: context) / 130,
+                  ),
+                  child: TextField(
+                    obscureText: true,
+                    keyboardType: TextInputType.visiblePassword,
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.vpn_key_rounded,
+                          color: Color(0xFF6F2DA8),
+                        ),
+                        focusedBorder: outlineInputBorder(),
+                        filled: true,
+                        fillColor: Colors.transparent,
+                        hintText: 'Password',
+                        contentPadding: EdgeInsets.all(8.0),
+                        border: outlineInputBorder()),
+                    onChanged: (val) {
+                      setState(() {
+                        passwordOnForgot = val;
+                      });
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: height(context: context) / 130,
+                  ),
+                  child: TextField(
+                    obscureText: true,
+                    keyboardType: TextInputType.visiblePassword,
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.vpn_key_rounded,
+                          color: Color(0xFF6F2DA8),
+                        ),
+                        errorText: isPasswordMatch == null || isPasswordMatch!
+                            ? null
+                            : 'Mismatched Password',
+                        focusedBorder: outlineInputBorder(),
+                        filled: true,
+                        fillColor: Colors.transparent,
+                        hintText: 'Confirm Password',
+                        contentPadding: EdgeInsets.all(8.0),
+                        border: outlineInputBorder()),
+                    onChanged: (val) {
+                      setState(() {
+                        confirmpasswordOnForgot = val;
+                        if (passwordOnForgot == confirmpasswordOnForgot) {
+                          isPasswordMatch = true;
+                        } else {
+                          isPasswordMatch = false;
+                        }
+                      });
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: height(context: context) / 20,
+                ),
+                MaterialButton(
+                  color: Color(0xFF6F2DA8),
+                  disabledColor: Colors.grey,
+                  minWidth: 163,
+                  height: 40,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  onPressed: passwordOnForgot != null &&
+                          confirmpasswordOnForgot != null &&
+                          passwordOnForgot != "" &&
+                          confirmpasswordOnForgot != "" &&
+                          passwordOnForgot != " " &&
+                          confirmpasswordOnForgot != " " &&
+                          isPasswordMatch != null &&
+                          isPasswordMatch!
+                      ? () {
+                          Navigator.of(context).pushAndRemoveUntil(
+                              MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                      LoginPage()),
+                              (Route<dynamic> route) => false);
+                        }
+                      : null,
+                  child: Text(
+                    'SAVE',
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
